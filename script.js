68f1bed2531942500d1051e6720d8a0a729ce0cb
Mithali <shrikantschannal@Mithalis-MacBook-Pro.local>
2024-09-27 16:17:56 +0530

    Your commit message

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 });
+$(document).ready(function() {
+    var containerWidth = $('.carousel-container').width();
+    var chevronOffset = 50; // How far out you want the chevrons to move
+    
+    $('.glyphicon-chevron-left').css('left', -chevronOffset + 'px');
+    $('.glyphicon-chevron-right').css('right', -chevronOffset + 'px');
+});
+
